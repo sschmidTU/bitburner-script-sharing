2daa8b2f28a06dc27c82c87cec7c34d988573ef7
sschmidTU <s.schmid@phonicscore.com>
2021-12-13 16:14:15 +0100

    add src folder, add watchServer.js script

---
+++ src/watchServer.js
@@ -0,0 +1,42 @@
+/** @param {NS} ns 
+ * @author simon
+ * **/
+export async function main(ns) {
+    ns.disableLog('ALL');
+
+    let server = "joesguns";
+    if (ns.args[0]) {
+        server = ns.args[0];
+    } else {
+        ns.tprint("usage: run watchServer.js [server]");
+        ns.tprint("using server " + server + " by default.");
+    }
+    
+    let money = 0;
+    while (true) {
+        const newMoney = ns.getServerMoneyAvailable(server);
+        if (newMoney !== money) { // only show money if it has changed
+            let newMoneyPercent = newMoney / ns.getServerMaxMoney(server);
+            newMoneyPercent *= 100;
+            newMoneyPercent = newMoneyPercent.toFixed(1);
+
+            ns.print(`${printTimeNow()} new money: ${newMoney.toLocaleString()} (${newMoneyPercent}%)`);
+
+            money = newMoney;
+        }
+        await ns.sleep(1000);
+    }
+}
+
+function printTimeNow() {
+    const nowDate = new Date(Date.now());
+    return printTime(nowDate);
+}
+
+function printTime(date) {
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    return `${date.getHours()}:${minutes}`;
+}
