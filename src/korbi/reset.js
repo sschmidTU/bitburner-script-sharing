c2bc5ce31a322276d2d55cc6ab25266ab738d1fc
Korbinian Eckstein <korbinian90@gmail.com>
2022-01-21 12:19:59 +0100

    change host to home on reset

--- src/korbi/reset.js
+++ src/korbi/reset.js
@@ -1,9 +1,12 @@
+import { updateOptions } from "./utilities"
+
 /** @param {NS} ns **/
 export async function main(ns) {
 	await ns.write("servers_money_factor.txt", '{"n00dles":1}', "w")
 	await ns.write("servers_hack_xp.txt", '{}', "w")
 	await ns.write("tasks.txt", '{"n00dles":"weaken"}', "w")
 	await ns.write("backdoors.txt", "", "w")
+	await updateOptions(ns, "host", "home")
 	ns.rm("factionRepGain.txt")
 	ns.run("setScriptHost.js", 1, "set")
 	ns.run("setOptions.js", 1, "set")
